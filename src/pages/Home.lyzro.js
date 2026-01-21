613c78c187b4b02851aa84a06bf3408ea3c46fae
najeebasad89 <najeebasad89@gmail.com>
2026-01-21 03:17:49 +0000

    Update README.md

---
+++ src/pages/Home.lyzro.js
@@ -0,0 +1,14 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+
+	// Write your Javascript code here using the Velo framework API
+
+	// Print hello world:
+	// console.log("Hello world!");
+
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
+
+	// Click "Run", or Preview your site, to execute your code
+
+});
---
+++ src/pages/masterPage.js
@@ -0,0 +1,7 @@
+// The code in this file will load on every page of your site
+
+$w.onReady(function () {
+	// Write your cod$w.onReady(function () {
+  document.body.style.cursor = "url('https://static.wixstatic.com/media/dd9ca0_79b744315f1648a7b096ab96fcc4f508~mv2.png'), auto";
+
+});
